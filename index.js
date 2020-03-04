711f8672713bf51545c1af5eb374423954c845fc
Beau Troxclair <beau.troxclair@gmail.com>
2020-03-04 16:42:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(arr1, arr2){
-  finalArr = [];
+  let finalArr = [];
   for(i=0;i<arr1.length();i++){
     finalArr.push(`${arr1[i]} plays ${arr2[i]}`)
   }
